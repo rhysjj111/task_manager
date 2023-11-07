c8437d4bf62796ee9c7866bfe195e1a1a2ef3728
Rhys Johnston <rhysjj111@gmail.com>
2023-11-07 10:57:14 +0000

    Add functionality to add a new task: 'add_task.html' created, form created to add new task, backend updated so new task is stored on the database.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,9 +3,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
-  document.addEventListener('DOMContentLoaded', function() {
+    // modal initialization
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    })
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
